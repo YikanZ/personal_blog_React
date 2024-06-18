0725ce69f4a912b42d031e2d9c73e76d7c535700
yikan <zhang.yikan1@northeastern.edu>
2024-06-17 22:24:19 -0400

    Remove hashpassword.js

--- backend/hashpassword.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const plainTextPassword = 'Jiangqian123';
-const salt = bcrypt.genSaltSync(10);
-const hashedPassword = bcrypt.hashSync(plainTextPassword, salt);
-
-console.log(hashedPassword); // Output the hashed password
